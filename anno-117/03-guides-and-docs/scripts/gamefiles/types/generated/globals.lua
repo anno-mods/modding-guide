8dd4063ca8ece03ce8a1504a1f52a32cc65bfbf0
Serpens66 <Serpens66@users.noreply.github.com>
2026-03-15 02:22:20 +0100

    lua game files

---
+++ anno-117/03-guides-and-docs/scripts/gamefiles/types/generated/globals.lua
@@ -0,0 +1,467 @@
+---@diagnostic disable: missing-fields
+---@meta global
+-- Generated File, do not edit (template Globals.stg)
+
+---@type rdgs.CAccountManager
+Account = {}
+---@type rdgs.CAccountSettings
+AccountSettings = {}
+---@type rdgs.CAchievementManager
+Achievements = {}
+---@type rdgs.CActiveTradeManager
+ActiveTrade = {}
+---@type rdgs.CActivitiesManager
+Activities = {}
+---@type rdgs.CAIConstructionManager
+AiConstruction = {}
+---@type rdgs.CAIUnitManager
+AiUnit = {}
+---@type rdgs.CAnimalManager
+Animals = {}
+---@type rdgs.CAppLifecycleFlow
+AppLifeCycleFlow = {}
+---@type rdgs.CAreaBuildabilityGridManager
+AreaBuildabilityGrid = {}
+---@type rdgs.CAreaFestivalManager
+Festival = {}
+---@type rdgs.CAreaFetchManager
+AreaFetch = {}
+---@type rdgs.CAreaIncidentManager
+Incidents = {}
+---temporary hack to not have to create the arealistmanager right now
+---@type rdgs.CAreaLoader
+Area = {}
+---@type rdgs.CAreaManager
+AreaManager = {}
+---@type rdgs.CAreaNeedAttributeManager
+AreaNeedAttributes = {}
+---@type rdgs.CAreaPopulationManager
+AreaPopulation = {}
+---@type rdgs.CAreaReligionManager
+AreaReligion = {}
+---@type rdgs.CAreaResidenceConsumptionManager
+AreaResidenceConsumption = {}
+---@type rdgs.CAreaTakeoverManager
+AreaTakeoverManager = {}
+---@type rdgs.CAreaWorkforceManager
+AreaWorkforce = {}
+---@type rdgs.CAssertManager
+Assert = {}
+---for backwards compatibility we will stick to the old name for now
+---@type rdgs.CAssetDataHelper
+ToolOneDataHelper = {}
+---@type rdtime
+FuelDuration = false
+---@type rdgs.CAssetDataManager
+AssetDataManager = {}
+---@type rdgs.CAutomatedTestManager
+AutomatedTest = {}
+---@type rdgs.CBenchmarkManager
+Benchmark = {}
+---@type rdgs.CBlueprintManager
+Blueprint = {}
+---@type rdgs.CBuildModeManager
+BuildMode = {}
+---@type rdgs.CCameraManager
+SessionCamera = {}
+---@type rdgs.CCameraSequenceManager
+CameraSequenceManager = {}
+---@type rdgs.CCheatBindings
+Cheats = {}
+---@type rdgs.CCheatManager
+Cheat = {}
+---@type rdgs.CCinematicsManager
+Cinematics = {}
+---@type rdgs.CConditionManager
+Conditions = {}
+---@type rdgs.CConditionVariableManager
+Variables = {}
+---@type rdgs.CContractManager
+Contracts = {}
+---@type rdgs.CCorporationDlcUpgradeManager
+DlcUpgrade = {}
+---@type rdgs.CDaytimeManager
+Daytime = {}
+---@type rdgs.CDebugInfoPageManager
+DebugInfo = {}
+---@type rdgs.CDebugRenderManager
+DebugRender = {}
+---@type rdgs.CDesyncRecoverFlow
+DesyncRecover = {}
+---@type rdgs.CDiplomacyManager
+Diplomacy = {}
+---@type rdgs.CDiscoveryManager
+Discovery = {}
+---@type rdgs.CEconomyManager
+Economy = {}
+---@type rdgs.CEconomyStatisticManager
+EconomyStatistic = {}
+---@type rdgs.CEngineMetricInfos
+EngineMetrics = {}
+---@type rdgs.CEnvironmentManager
+Environment = {}
+---@type rdgs.CFetchSavegameFlow
+SavegameFetcher = {}
+---@type rdgs.CFrameGrabber
+FrameGrabber = {}
+---@type rdgs.CFullscreenMovieQueueManager
+FullscreenMovieQueueManager = {}
+---returns the singleton instance
+---@type rdgs.CGameClock
+GameClock = {}
+---@type rdgs.CGameManager
+Game = {}
+---@type rdgs.CGameObjectManager
+Objects = {}
+---@type rdgs.CGamepadActionManager
+GamepadActionManager = {}
+---@type rdgs.CGameRuntimeSettings
+RuntimeSettings = {}
+---@type rdgs.CGameSessionManager
+GameSession = {}
+---@type rdgs.CGameSetupManager
+GameSetup = {}
+---@type rdgs.CGovernorDecisionManager
+GovernorDecision = {}
+---@type rdgs.CHallOfFameManager
+HallOfFame = {}
+---@type rdgs.CHappyDayEventManager
+HappyDayEventManager = {}
+---@type rdgs.CInfoTipManager
+InfoTip = {}
+---@type rdgs.CInputManager
+Input = {}
+---@type rdgs.CIrrigationManager
+Irrigation = {}
+---@type rdgs.CIslandManager
+Island = {}
+---@type rdgs.CItemSessionManager
+Item = {}
+---@type rdgs.CLanguageManager
+Language = {}
+---we want GetManagerMutable inline and TextSourceParser does not support it yet to define it here, so its defined below in this .h
+---@type rdgs.CLoadingPierManager
+LoadingPier = {}
+---@type rdgs.CMemorizeManager
+Memorize = {}
+---@type rdgs.CMemoryTrackingManager
+MemoryTracking = {}
+---@type rdgs.CMetaEffectManager
+MetaEffects = {}
+---@type rdgs.CMetaGameManager
+MetaGame = {}
+---@type rdgs.CMetaGameObjectManager
+MetaObjects = {}
+---@type rdgs.CMetaIncidentManager
+MetaIncidents = {}
+---@type rdgs.CMetaMoneyManager
+Money = {}
+---returns tech handler for local peer
+---@type rdgs.CMetaPropertyIslandOwner
+IslandOwner = {}
+---@type rdgs.CMetaPropertyNotesHandler
+Notes = {}
+---returns tech handler for local peer
+---@type rdgs.CMetaPropertyTechHandler
+TechHandler = {}
+---@type rdgs.CMetaReligionManager
+Religion = {}
+---@type rdgs.CMetaUnitManager
+Unit = {}
+---@type rdgs.CModManager
+Mods = {}
+---@type rdgs.CMouseCursorManager
+MouseManager = {}
+---@type rdgs.CMPReloadFlow
+MPReload = {}
+---@type rdgs.CNotificationManager
+Notifications = {}
+---@type rdgs.CObjectSpawnManager
+ObjectSpawn = {}
+---@type rdgs.COnlineManager
+Online = {}
+---@type rdgs.COptionsManager
+Options = {}
+---@type rdgs.CParticipantManager
+Participants = {}
+---Returns whether the passive trade feature is enabled
+---@type boolean
+IsPassiveTradeFeatureEnabled = false
+---@type rdgs.CPathManager
+Path = {}
+---@type rdgs.CPauseManager
+Pause = {}
+---@type rdgs.CPerformanceTrackingManager
+PerformanceTracking = {}
+---@type rdgs.CPinManager
+Pins = {}
+---@type rdgs.CPopulationManager
+Population = {}
+---@type rdgs.CPopupManager
+Popup = {}
+---@type rdgs.CQuestManager
+Quests = {}
+---@type rdgs.CRegrowManager
+Regrow = {}
+---@type rdgs.CRemoteImageManager
+RemoteImage = {}
+---@type rdgs.CRewardsManager
+Rewards = {}
+---@type rdgs.CRomanizationManager
+Romanization = {}
+---@type rdgs.CSavegameManager
+Savegame = {}
+---@type rdgs.CScenarioManager
+Scenarios = {}
+---@type rdgs.CScriptBindings
+Scripts = {}
+---@type rdgs.CSeasonManager
+Season = {}
+---@type rdgs.CSelectionManager
+Selection = {}
+---@type rdgs.CSessionAqueductManager
+SessionAqueduct = {}
+---@type rdgs.CSessionCoastManager
+AreaCoast = {}
+---@type rdgs.CSessionCooldownManager
+Cooldown = {}
+---@type rdgs.CSessionEffectManager
+SessionEffects = {}
+---@type rdgs.CSessionFeedbackManager
+Feedback = {}
+---@type rdgs.CSessionFreeAreaProductivityManager
+FreeAreaProductivity = {}
+---@type rdgs.CSessionMiscRenderManager
+MiscRender = {}
+---@type rdgs.CSessionParticipantManager
+SessionParticipants = {}
+---@type rdgs.CSessionStreetManager
+Street = {}
+---@type rdgs.CSessionUnitManager
+SessionUnit = {}
+---@type rdgs.CShipModuleConfigurationManager
+ShipModuleConfiguration = {}
+---@type rdgs.CShortcutManager
+ShortcutManager = {}
+---@type rdgs.CSlotManager
+Slot = {}
+---@type rdgs.CSoundManager
+Sound = {}
+---@type rdgs.CStreetOverlayManager
+StreetOverlay = {}
+---@type rdgs.CTargetManager
+Target = {}
+---@type rdgs.CTextManager
+Text = {}
+---@type rdgs.CToolOneTextHelper
+ToolOneHelper = {}
+---@type rdgs.CTradeRouteManager
+TradeRoute = {}
+---@type rdgs.CUnlockManager
+Unlock = {}
+---@type rdgs.CVolcanoEruptionManager
+Volcano = {}
+---@type rdgs.CWeatherManager
+Weather = {}
+---@type rdgs.CWinLoseManager
+WinLose = {}
+---@type rdgs.CWorldManager
+World = {}
+---@type rdgs.CWorldMapSessionManager
+WorldMap = {}
+---@return rdgs.CAchievementAsset
+function GetAchievementAssetData() end
+---@return rdgs.CAreaFestivalManager
+function FestivalFromArea() end
+---@return rdtime
+function GetFestivalDuration() end
+---@return boolean
+function IsIncidentAttributeLowEnoughToTriggerNotification() end
+---@return rdgs.CAreaManager
+function GetAreaManagerByIslandGUID() end
+---@return rdgs.CAreaManager
+function GetAreaManagerByID() end
+function SetEnableObjectGroup() end
+function OpenTakeoverDecisionUI() end
+---@return boolean
+function IsAreaStillVassalized() end
+---Returns true if we have  0 workforce of this type
+---@return boolean
+function IsWorkforceMissing() end
+---@return rdgs.CAsset
+function AssetData() end
+---@return rdgs.ProductGUID
+function GetProductFromNeed() end
+---@return integer
+function GetNeedCategoryFromNeed() end
+---@return boolean
+function IsValidNeed() end
+---@return boolean
+function IsHumanParticipant() end
+---@return boolean
+function IsSecondPartyParticipant() end
+---@return boolean
+function IsThirdPartyParticipant() end
+---@return boolean
+function IsVirtualParticipant() end
+---@return boolean
+function IsPirateParticipant() end
+---@return boolean
+function IsTraderParticipant() end
+---@return boolean
+function IsEmperorRepresentativeParticipant() end
+---@return boolean
+function IsEmperorParticipant() end
+---@return boolean
+function IsFuelProduct() end
+---add a value to a storage with the name given
+function AddBenchValue() end
+---add a tag to a storage with the name given
+function AddBenchTag() end
+---Returns a vector of goods/items/money the player receives if this decision gets active
+---@return rdgs.DemandedGoodEx[]
+function GetDecisionOutcomeMaterial() end
+---Returns true if the outcome materials are items ore goods
+---@return boolean
+function HasNonMetaDecisionOutcomeMaterial() end
+---Returns a guid of a countermeasure for incidents when this decision gets active
+---@return integer
+function GetDecisionOutcomeIncidentCountermeasure() end
+---Returns a list of guids of any effects which will be started when this decision gets active
+---@return integer[]
+function GetDecisionOutcomeEffectStarted() end
+---Returns true if this decision will start a effect on a specific location
+---@return boolean
+function HasDecisionOutcomeEffectLocationInfos() end
+---Returns the name of the location if this decision will start a effect on a specific location
+---@return string
+function GetDecisionOutcomeEffectLocationInfo() end
+---@return rdgs.CDiplomacyGift
+function DiplomacyGift() end
+---@return rdgs.CDiplomacyRansom
+function DiplomacyRansom() end
+function HideUndiscoverVisuals() end
+---Returns the effect if it exists
+---@return rdgs.CEffect|nil
+function GetEffect() end
+---@return boolean
+function IsEffect() end
+---Returns a vector of all the value upgrades a buff contains (containing icons, text and values), does not include some upgrades like pirate flag, white flag etc.
+---@return rdgs.CTextSourceListValue[]
+function GetBuffDetails() end
+---@return rdgs.CTextSourceListValue[]
+function GetStaticBuffDetails() end
+---Returns a vector of all the value upgrades of all buffs affecting this objects health (containing icons, text and values)
+---@return rdgs.CTextSourceListValue[]
+function GetHealthModifiers() end
+---@return rdgs.CTextSourceListValue[]
+function GetAttributeModifiers() end
+---Returns the time left if a effect id is given or the duration of the effect if only the guid is given
+---@return rdtime
+function GetEffectTimeLeft() end
+---@return rdgs.CFactoryAsset
+function GetFactoryAssetData() end
+---@return rdgs.CGameObject
+function GetGameObject() end
+---@return rdgs.CItemAsset
+function ItemAssetData() end
+---@return rdgs.CLoanState
+function LoanState() end
+function DisableAllMajorIncidents() end
+---NAME
+---@return string
+function GetCompanyNameForParticipant() end
+---PORTRAIT
+---@return string
+function GetAvatarPortraitIconForParticipant() end
+---@return integer
+function GetAvatarPortraitForParticipant() end
+---LOGO
+---@return integer
+function GetCompanyLogoGUIDForParticipant() end
+---RATINGS
+---@return rdgs.RivalRating
+function GetRatingForParticipant() end
+---@return rdgs.CPopulationAsset
+function GetPopulationAssetData() end
+---@return rdgs.CProductAsset
+function GetProductAssetData() end
+---@return boolean
+function IsProduct() end
+---@return boolean
+function IsBuilding() end
+---@return boolean
+function IsFreeAreaBuilding() end
+---@return datasets.FreeAreaType
+function GetFreeAreaType() end
+function ToggleEnable() end
+---@return number
+function GetLoadingSpeedFactorForGUID() end
+---Cheat to starts a new storyline in the correct manager. Decisions - governor decision manager, Contracts - Contract manager, Quest - questmanager
+function CheatStartStoryLineForCurrentPlayerNet() end
+---@return rdgs.CQuestComponent
+function GetQuestComponent() end
+---Returns the components location info for a given componentID, if it doesn\'t exist the request will be passed on to the parent component until a location was found or we reached the topmost parent
+---@return rdgs.CLocationInfo|nil
+function GetLocationInfo() end
+---@return rdgs.CRecipeAsset
+function GetRecipeAssetData() end
+---Utility
+---@return rdgs.CAsset
+function GetAssetForValue() end
+---@return boolean
+function And() end
+---@return boolean
+function Not() end
+---@return boolean
+function Greater() end
+---@return boolean
+function GreaterThanZero() end
+---Returns the produced product from the endbuilding of the chain or the public service building\nReturns INVALID_GUID if the guid is not a chain, production building or public building
+---@return integer
+function GetProductFromChain() end
+---@return number
+function GetNeedAttributeValueByProduct() end
+---returns the count of attributes where the value is != 0
+---@return integer
+function GetNeedAttributesCountByProduct() end
+---@return number
+function GetNeedAttributeValueByNeed() end
+---@return number
+function GetNeedSupplyByProduct() end
+
+---@class Static Lists all virtual classes. A virtual class bundles some methods of the same context but of different classes in a namespace.
+---@field AqueductConsumer rdgs.VirtualSpaceAqueductConsumer
+---@field Attributes rdgs.VirtualSpaceAttributes
+---@field Battles rdgs.VirtualSpaceBattles
+---@field BuildCost rdgs.VirtualSpaceBuildCost
+---@field Building rdgs.VirtualSpaceBuilding
+---@field CombatPlanner rdgs.VirtualSpaceCombatPlanner
+---@field Effect rdgs.VirtualSpaceEffect
+---@field Factory rdgs.VirtualSpaceFactory
+---@field Festival rdgs.VirtualSpaceFestival
+---@field Health rdgs.VirtualSpaceHealth
+---@field Incident rdgs.VirtualSpaceIncident
+---@field Maintenance rdgs.VirtualSpaceMaintenance
+---@field NotificationReward rdgs.VirtualSpaceNotificationReward
+---@field Pier rdgs.VirtualSpacePier
+---@field PublicService rdgs.VirtualSpacePublicService
+---@field RepairCrane rdgs.VirtualSpaceRepairCrane
+---@field Reputation rdgs.VirtualSpaceReputation
+---@field Romanization rdgs.VirtualSpaceRomanization
+---@field Scenario rdgs.VirtualSpaceScenario
+---@field Seasons rdgs.VirtualSpaceSeasons
+---@field SettlementRight rdgs.VirtualSpaceSettlementRight
+---@field ShipModuleConfiguration rdgs.VirtualSpaceShipModuleConfiguration
+---@field Ships rdgs.VirtualSpaceShips
+---@field Tags rdgs.VirtualSpaceTags
+---@field TechHandler rdgs.VirtualSpaceTechHandler
+---@field Trade rdgs.VirtualSpaceTrade
+---@field Trader rdgs.VirtualSpaceTrader
+---@field TransferActions rdgs.VirtualSpaceTransferActions
+---@field UniqueBuildings rdgs.VirtualSpaceUniqueBuildings
+---@field Units rdgs.VirtualSpaceUnits
+---@field Villa rdgs.VirtualSpaceVilla
+---@field Warehouse rdgs.VirtualSpaceWarehouse
+---@field Workforce rdgs.VirtualSpaceWorkforce
+Static = {}
